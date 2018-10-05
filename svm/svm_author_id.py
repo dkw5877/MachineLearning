6a5f27dcf9051aab0e9f477a197dc1e081d65f0c
dennis-walsh <intrnetmail@yahoo.com>
2018-10-05 13:09:29 -0700

    support vector machine mini project 2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,11 +19,41 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+### reduce data set size
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+###train the classifier using Support Vector Classification (SVC)
+#set gamma for rbf to avoid warning 
+
+t0 = time()
+classifier = svm.SVC(kernel='rbf', gamma='scale', C=100)
+classifier.fit(features_train, labels_train)
+print "training time = ", round(time() -  t0, 3), "s"
+
+##make predictions
+t0 = time()
+predictions = classifier.predict(features_test)
+print "prediction time = ", round(time() - t0, 3), "s"
+
+answer1 = predictions[9]
+print "10th = ", answer1
+
+answer2 = predictions[25]
+print "26th = ", answer2
+
+answer3 = predictions[49]
+print "50th = ", answer3
+
+
+##accuracy
+accuracy = accuracy_score(labels_test, predictions)
+print "accuracy score: ", accuracy
 
 #########################################################
 
