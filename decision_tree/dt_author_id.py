4ea32b1af553ba32d7cbf38fa6a0249a3ed8b971
dennis-walsh <intrnetmail@yahoo.com>
2018-10-08 10:37:24 -0700

    decision trees mini project 3

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+#features
+numcolumns = len(features_train[0])
+print "columns = ", numcolumns
+
+##training
+classifier = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+##predictions
+t0 = time()
+predictions = classifier.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+##accuracy
+accuracy = accuracy_score(predictions, labels_test)
+print "accuracy = ", round(accuracy,3)
 
 #########################################################
+#prettyPicture(clf, features_test, labels_test)
+#output_image("test.png", "png", open("test.png", "rb").read())
 
 
