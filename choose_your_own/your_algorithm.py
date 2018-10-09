c45a6ef34357549b683cff4b6c3113d13b8a4e06
dennis-walsh <intrnetmail@yahoo.com>
2018-10-09 14:24:05 -0700

    RandomForest, Adaboost, K-NearestNeighbor mini project 4

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
+import sys
+from time import time
+sys.path.append("../tools/")
+
+#required imports
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
-from class_vis import prettyPicture
+from class_vis import prettyPicture, output_image
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -27,18 +32,50 @@ plt.ylabel("grade")
 plt.show()
 ################################################################################
 
-
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score
+import numpy as numpy
 
+##training
+neighbors = ['auto', 'ball_tree', 'kd_tree', 'brute']
+adaboosts = ['SAMME.R', 'SAMME']
+classifiers = ['RandomForest', 'AdaBoost', 'K-NearestNeighbor']
+classifierType = classifiers[2]
 
+if classifierType == 'RandomForest':
+    clf = RandomForestClassifier(n_estimators=10, min_samples_leaf=1)
+elif classifierType == 'AdaBoost':
+    clf = AdaBoostClassifier(algorithm=adaboosts[0], n_estimators=50, learning_rate=1.0)
+elif classifierType == 'K-NearestNeighbor':
+    clf = KNeighborsClassifier(algorithm=neighbors[0], n_neighbors=5, leaf_size=30)
+else:
+    clf = GaussianNB()
 
+print "classifier:", clf
+t0 = time()
+print "training classifier..."
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+##predictions
+t0 = time()
+print "making predictions..."
+predictions = clf.predict(features_test)
+print "predictions time:", round(time()-t0, 3), "s"
 
+##accuracy
+accuracy = accuracy_score(predictions, labels_test)
+print "accuracy: ", accuracy
 
-
-
+### visualization code (prettyPicture) to show you the decision boundary
+fileName = classifierType + ".png"
 try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+    prettyPicture(clf, features_test, labels_test, fileName)
+except NameError as e:
+    print e
