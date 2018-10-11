55f1dc7e4f794542e40f479c5d19c1279757cc03
dennis-walsh <intrnetmail@yahoo.com>
2018-10-11 10:25:36 -0700

    linear regression mini project 6

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -26,10 +26,11 @@ data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +38,13 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
-
-
-
-
+print "r-square:  ", reg.score(feature_train, target_train)
+print "slope:     ", reg.coef_
+print "intercept: ", reg.intercept_
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
