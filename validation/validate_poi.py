29df888eb0424378d0dab711b4ae525f68453f0e
dennis-walsh <intrnetmail@yahoo.com>
2018-10-26 14:46:24 -0700

    valiation mini project 13

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,8 +12,10 @@
 
 import pickle
 import sys
+from time import time
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+sort_keys = '../tools/python2_lesson13_keys.pkl'
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -25,8 +27,27 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
 ### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=1)
+
+##training
+clf = DecisionTreeClassifier()
+t0 = time()
+clf.fit(features, labels)
+print "training time:", round(time()-t0, 3), "s"
+
+##predictions
+t0 = time()
+predictions = clf.predict(features)
+print "prediction time:", round(time()-t0, 3), "s"
+
+##accuracy
+## quiz expected answer =  0.9894736842105263
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(predictions, labels)
+print "accuracy = ", accuracy
 
 
